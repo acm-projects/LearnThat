7f99abe310154acf4073345a522722399588c546
Ranvir Patil <ranvir.patil@hotmail.com>
2024-11-01 21:23:33 -0500

    Revert "Merge branch 'main' of https://github.com/acm-projects/LearnThat"
    This reverts commit 3eb1cc6a8bc4b7dae34a5b85d4ac747e9f4251f6, reversing
    changes made to ca56b6f2c813d7684ecea71aa3ce27a800f70264.

--- src/webpageApp.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./webpageApp.css";
-import Webpage from "./Webpage.tsx";
-
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-createRoot(document.getElementById("root")!).render(<Webpage />);
